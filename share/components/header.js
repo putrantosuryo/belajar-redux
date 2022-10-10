b28a6281059017ba6303a9d625853a0cc80bc08c
suryo <putrantosuryo@gmail.com>
2022-10-10 21:32:21 +0700

    done task

--- share/components/header.js
+++ share/components/header.js
@@ -1,12 +1,17 @@
 import Link from "next/link";
 import React, { useEffect } from "react";
 import { useCookies } from "react-cookie";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { setLogin, setLogout } from "../redux/authSlice";
 
 const Header = () => {
-  const [cookies] = useCookies(["accessToken", "userId"]);
+  const [cookies] = useCookies([
+    "accessToken",
+    "userId",
+    "email",
+  ]);
   const dispatch = useDispatch();
+  const auth = useSelector((state) => state.auth);
 
   useEffect(() => {
     if (cookies.accessToken) {
@@ -15,9 +20,35 @@ const Header = () => {
   }, []);
 
   const handleLogout = () => {
+    document.cookie =
+      "accessToken=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+    document.cookie = "userId=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+    document.cookie =
+      "email=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+    window.location.reload();
     console.log("proses logout");
     dispatch(setLogout());
   };
+  let menu;
+  if (!auth.isLoggedIn) {
+    menu = (
+      <li className="nav-item">
+        <Link href="/login">
+          <a className="nav-link">Login</a>
+        </Link>
+      </li>
+    );
+  } else {
+    menu = (
+      <li
+        className="nav-item"
+        style={{ cursor: "pointer" }}
+        onClick={handleLogout}
+      >
+        <a className="nav-link">Logout</a>
+      </li>
+    );
+  }
 
   return (
     <nav className="navbar navbar-expand-lg bg-light">
@@ -37,18 +68,7 @@ const Header = () => {
             <a className="nav-link">Users</a>
           </Link>
         </li>
-        <li className="nav-item">
-          <Link href="/login">
-            <a className="nav-link">Login</a>
-          </Link>
-        </li>
-        <li
-          className="nav-item"
-          style={{ cursor: "pointer" }}
-          onClick={handleLogout}
-        >
-          <a className="nav-link">Logout</a>
-        </li>
+        {menu}
       </ul>
     </nav>
   );
